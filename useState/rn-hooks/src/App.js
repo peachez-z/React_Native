5d9efd1af6bf78bdb26ecb72c0caaab20c33f343
peachez-z <rkfld0911@gmail.com>
2023-10-27 16:19:04 +0900

    useState 완료'

---
+++ useState/rn-hooks/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components/native";
+import Counter from "./component/Counter";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default function App() {
+  return (
+    <Container>
+      <Counter />
+    </Container>
+  );
+}
