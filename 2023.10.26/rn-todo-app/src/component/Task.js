90d71dfe89392adaa21aa5ba71c82d0015969133
peachez-z <rkfld0911@gmail.com>
2023-10-27 09:55:35 +0900

    체크박스 완료

--- 2023.10.26/rn-todo-app/src/component/Task.js
+++ 2023.10.26/rn-todo-app/src/component/Task.js
@@ -22,10 +22,14 @@ const Contents = styled.Text`
   padding: 0 20px;
 `;
 
-const Task = ({ item, deleteTask }) => {
+const Task = ({ item, deleteTask, toggleTask }) => {
   return (
     <Container>
-      <IconButton icon={icons.uncheck} />
+      <IconButton
+        icon={item.completed ? icons.check : icons.uncheck}
+        id={item.id}
+        onPress={toggleTask}
+      />
       <Contents>{item.text}</Contents>
       <IconButton icon={icons.edit} />
       <IconButton icon={icons.delete} id={item.id} onPress={deleteTask} />
@@ -36,6 +40,7 @@ const Task = ({ item, deleteTask }) => {
 Task.propTypes = {
   item: PropTypes.object.isRequired,
   deleteTask: PropTypes.func.isRequired,
+  toggleTask: PropTypes.func.isRequired,
 };
 
 export default Task;
